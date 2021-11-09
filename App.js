1659a1226e34a48344506d641dd9e0afe622d708
Tim Burch <tim@linchpin.software>
2021-11-09 16:54:11 -0500

    Worked on landing page

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // @todo on load: pause for 1 sec(?) before loading main menu
+  // @todo dashboard: food, water, and fuel for x days; water is LCDs
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titleText}>Preppy</Text>
+      <Text style={styles.subtitle}>Don't be scared, be prepared</Text>
+      <View style={styles.buttonWrapper}>
+        <Button style={styles.button} title="Profile"/>
+        <Button style={styles.button} title="Inventory"/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    width: '48%',
+  },
+  buttonWrapper: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
+    margin: 10,
+  },
+  subtitle: {
+    marginBottom: 20,
+  },
+  titleText: {
+    flexDirection: 'row',
+    fontSize: 24,
   },
 });
