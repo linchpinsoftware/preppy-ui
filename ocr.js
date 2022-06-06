44041ba7dd7bdbb58d425c39c57eaaaf0faff4c2
Tim Burch <38444603+linchpinsoftware@users.noreply.github.com>
2022-06-06 18:19:31 -0400

    Update ocr.js

--- ocr.js
+++ ocr.js
@@ -9,8 +9,8 @@ const datafs = fs.readFileSync("./assets/cacao_facts.png");
 const client = new TextractClient({
   region: 'us-east-1',
   credentials: {
-    accessKeyId: 'AKIASV7CDFJONFKYMMEE',
-    secretAccessKey: '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',
+    accessKeyId: ':access_key_id',
+    secretAccessKey: ':secret_access_key',
   }
 });
 
